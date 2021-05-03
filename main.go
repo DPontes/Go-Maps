759ca76b1a4c7413704a19344e426728417edd20
DPontes <dpontes@protonmail.com>
2021-05-03 11:25:13 +0200

    Implemented function to iterate and print out contents of map

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
+
+  // Declaring a map with values
   colors := map[string]string {     // this map will have ´keys´ of type string and `values` of type string
     "red"   : "#FF0000",
     "green" : "#0000FF",
     "blue"  : "#00FF00",
   }
 
-  fmt. Println(colors)
+  printMap(colors)
+}
+
+func printMap(c map[string]string) {
+  for color, hex := range c {
+    fmt.Println("hex code for", color, "is", hex)
+  }
 }
